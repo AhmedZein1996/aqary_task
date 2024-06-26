d015234d3c90965682edcacb70c0557130383064
Ahmed Zein <ahmedzezo1080@gmail.com>
2024-06-26 12:50:08 +0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	test/app_test.dart

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,58 +1,59 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:github_task/features/trending_repos/data/datasources/github_trending_repo_datasource.dart';
 import 'package:github_task/features/trending_repos/data/model/trending_repo_model.dart';
 import 'package:github_task/features/trending_repos/data/repositories/github_trending_repository.dart';
 import 'package:github_task/features/trending_repos/logic/trending_repo/trending_repo_bloc.dart';
 import 'package:mocktail/mocktail.dart';
 
+// Mock repository class for testing
 class MockGithubTrendingRepository extends Mock
     implements GithubTrendingRepository {}
 
-class MockGithubTrendingDatasource extends Mock
-    implements BaseGithubTrendingRepoDatasource {}
-
 void main() {
-  late TrendingRepoBloc sut;
-  late TrendingRepoEvent event;
-  late MockGithubTrendingDatasource mockGithubTrendingDatasource;
-  late MockGithubTrendingRepository mockGithubTrendingRepository;
-  setUp(() {
-    mockGithubTrendingDatasource = MockGithubTrendingDatasource();
-    mockGithubTrendingRepository = MockGithubTrendingRepository();
-    event = GetTrendingRepoEvent();
-    sut = TrendingRepoBloc(mockGithubTrendingRepository);
-  });
-  final List<TrendingRepoModel> testRepos = [
-    const TrendingRepoModel('trending 1', 'description 1',
-        {'__typename': 'Language', 'name': 'Shell'}),
-    const TrendingRepoModel('trending 2', 'description 2',
-        {'__typename': 'Language', 'name': 'Python'}),
-    const TrendingRepoModel(
-        'trending 3', 'description 3', {'__typename': 'Language', 'name': 'C'}),
-  ];
-  void arrangeTrendingRepositoryReturns3Reps() {
-    when(() => mockGithubTrendingRepository.getTrendingRepo())
-        .thenAnswer((_) async => Right(testRepos));
-  }
+  group('TrendingRepoBloc', () {
+    late TrendingRepoBloc sut;
+    late MockGithubTrendingRepository mockGithubTrendingRepository;
 
-  test(
-    'initial values are correct',
-    () {
-      expect(sut.repos, []);
-      expect(sut.state.runtimeType, TrendingRepoInitialState);
-    },
-  );
+    setUp(() {
+      mockGithubTrendingRepository = MockGithubTrendingRepository();
+      sut = TrendingRepoBloc(mockGithubTrendingRepository);
+    });
 
-  // Get Top Stories And Filter Stories Test
+    tearDown(() {
+      sut.close();
+    });
 
-  group('getRepos', () {
     test(
       'gets trending repos',
       () async {
+        // Arrange
+        final event = GetTrendingRepoEvent();
+        final List<TrendingRepoModel> repos = [
+          const TrendingRepoModel('trending 1', 'description 1',
+              {'__typename': 'Language', 'name': 'Shell'}),
+          const TrendingRepoModel('trending 2', 'description 2',
+              {'__typename': 'Language', 'name': 'Python'}),
+          const TrendingRepoModel('trending 3', 'description 3',
+              {'__typename': 'Language', 'name': 'C'}),
+        ];
+        when(() => mockGithubTrendingRepository.getTrendingRepo())
+            .thenAnswer((_) async => Right(repos));
+
+        // Act
         sut.add(event);
+
+        // Assert
+        await expectLater(
+          sut.stream,
+          emitsInOrder([
+            isA<TrendingRepoLoadingState>(),
+            isA<TrendingRepoSuccessState>(),
+          ]),
+        );
+
+        expect((sut.state as TrendingRepoSuccessState).repos.length,
+            3); // Adjust this expectation based on your test scenario
         verify(() => mockGithubTrendingRepository.getTrendingRepo()).called(1);
-        expect(sut.repos.length, 3);
       },
     );
   });
